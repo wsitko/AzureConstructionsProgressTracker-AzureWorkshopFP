672f630a612cdb0816724457b7ba02ba5538b4b9
wsitko <joystus@wp.pl>
2015-11-17 10:57:46 +0100

    added images upload support

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace AzureConstructionsProgressTracker.Features.ProgressTracking
 {
@@ -11,6 +14,17 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
         
         public async Task<string> UploadFile(string fileName, HttpPostedFileBase file)
         {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("test");
+            await container.CreateIfNotExistsAsync();
+            await container.SetPermissionsAsync(new BlobContainerPermissions  {     PublicAccess =  BlobContainerPublicAccessType.Blob   });
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(file.InputStream);
+            return blockBlob.Uri.ToString();
+
+
+
             // TODO: 
             // https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#programmatically-access-blob-storage
             // - Add proper nuget
@@ -18,7 +32,7 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - create container
             // - upload blob
             // - return URL
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
